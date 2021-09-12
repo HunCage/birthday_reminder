db8a0108cf8e9fba076e06144a9230daeced92d3
Peter Horvath <gkipeter@gmail.com>
2021-09-12 18:37:29 +0200

    deploy

--- src/List.js
+++ src/List.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
 
-const List = () => {
-  return (
-    <>
-      <h2>list component</h2>
-    </>
-  );
+const List = ({ people }) => {
+    return (
+        <>
+            {people.map((person) => {
+              const {id, name, age, image} = person
+                return (
+                    <article key={id} className="person">
+                        <img src={image} alt="pic" />
+                        <div>
+                            <h5>{name}</h5>
+                            <p>{age}</p>
+                        </div>
+                    </article>
+                );
+            })}
+        </>
+    );
 };
 
 export default List;
